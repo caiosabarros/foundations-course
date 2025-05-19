8c4ecfe1ca5e68be2bfb2c5932b85112a0a82622
caiohenriquesa1@gmail.com <caiohenriquesa1@gmail.com>
2025-05-19 13:36:42 -0300

    redirected to the /dashboard/invoices route on invoices creation submission && see the new invoice at the top of the table.

---
+++ nextjs-dashboard/app/lib/actions.ts
@@ -0,0 +1,49 @@
+'use server';
+import { z } from 'zod';
+import postgres from 'postgres';
+/**
+ * Next.js has a client-side router cache that stores the route segments in the user's browser for a time. Along with prefetching, this cache ensures that users can quickly navigate between routes while reducing the number of requests made to the server.
+
+Since you're updating the data displayed in the invoices route, you want to clear this cache and trigger a new request to the server. You can do this with the revalidatePath function from Next.js
+ */
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+/**
+ * By adding the 'use server', you mark all the exported functions within the file as Server Actions.
+ *  These server functions can then be imported and used in Client and Server components. 
+ * Any functions included in this file that are not used will be automatically removed from the final application bundle.
+ * 
+ */
+
+export async function createInvoice(formData: FormData) {
+    // if I log this, it logs on the server, not the client browser.
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
